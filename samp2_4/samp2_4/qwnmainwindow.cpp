e908aca7d6414112168deecd707dd18f92718034
LONGZR007 <1558193230@qq.com>
2019-09-18 23:28:32 +0800

    增加图标，增加手写代码spinBox，和进度条字体选择的使用

--- samp2_4/samp2_4/qwnmainwindow.cpp
+++ samp2_4/samp2_4/qwnmainwindow.cpp
@@ -6,10 +6,47 @@ QWNMainWindow::QWNMainWindow(QWidget *parent)
     , ui(new Ui::QWNMainWindow)
 {
     ui->setupUi(this);
+    iniUI();
 }
 
 QWNMainWindow::~QWNMainWindow()
 {
     delete ui;
+}
+
+void QWNMainWindow::iniUI()
+{
+    /* 在状态栏上增加标签 */
+    fLabCurFile = new QLabel;
+    fLabCurFile->setMinimumWidth(150);
+    fLabCurFile->setText("当前文件:");
+    ui->statusBar->addWidget(fLabCurFile);    // 增加标签到状态栏
+
+    /* 在状态栏上增加进度条 */
+    progressBarq = new QProgressBar;
+    progressBarq->setMaximumWidth(200);
+    progressBarq->setMaximum(50);
+    progressBarq->setMinimum(5);
+    progressBarq->setValue(ui->textEdit->font().pointSize());
+    ui->statusBar->addWidget(progressBarq);
+
+    /* 在工具栏上增加SpinBox */
+    spinFontSize = new QSpinBox;
+    spinFontSize->setMinimum(5);
+    spinFontSize->setMaximum(50);
+    spinFontSize->setValue(ui->textEdit->font().pointSize());
+    spinFontSize->setMinimumWidth(50);
+    ui->toolBar->addWidget(new QLabel("字体大小"));
+    ui->toolBar->addWidget(spinFontSize);
+
+    ui->toolBar->addSeparator();     // 在工具栏增加分栏符
+
+    /* 增加字体选择 */
+    ui->toolBar->addWidget(new QLabel(" 字体 "));
+    comboFont = new QFontComboBox;
+    comboFont->setMinimumWidth(150);
+    ui->toolBar->addWidget(comboFont);
+
+    setCentralWidget(ui->textEdit);     // 这句是干嘛的？
 }
 
--- samp2_4/samp2_4/qwnmainwindow.h
+++ samp2_4/samp2_4/qwnmainwindow.h
@@ -2,6 +2,10 @@
 #define QWNMAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QProgressBar>
+#include <QSpinBox>
+#include <QFontComboBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class QWNMainWindow; }
@@ -17,5 +21,12 @@ public:
 
 private:
     Ui::QWNMainWindow *ui;
+
+private:
+    QLabel *fLabCurFile;            // 状态栏里显示当前文件的Label
+    QProgressBar *progressBarq;     // 在状态栏显示的进度条
+    QSpinBox *spinFontSize;         // 字体大小 spinBox
+    QFontComboBox *comboFont;       // 字体名称
+    void iniUI();
 };
 #endif // QWNMAINWINDOW_H
